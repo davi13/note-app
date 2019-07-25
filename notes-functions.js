c64ea2932b1ceeb03ae71b4b3bfaf7669dd2234d
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-07-25 08:49:07 +0200

    adding some class css with js, to new paragraph

--- notes-functions.js
+++ notes-functions.js
@@ -103,15 +103,23 @@ const sorteNotes = (notes, sortBy) => {
 
 // Render application notes
 const renderNotes = (notes, filters) => {
+    const notesEl = document.querySelector('#notes');
     notes = sorteNotes(notes, filters.sortBy);
     const filteredNotes = notes.filter((note) => note.title.toLowerCase().includes(filters.searchText.toLowerCase()));
 
-    document.querySelector('#notes').innerHTML = ''
+    notesEl.innerHTML = ''
+    if (filteredNotes.length > 0) {
+        filteredNotes.forEach((note) => {
+            const noteEl = generateNoteDOM(note)
+            notesEl.appendChild(noteEl)
+        })
+    } else {
+        const emptyMessage = document.createElement('p');
+        emptyMessage.textContent = 'No notes to show';
+        emptyMessage.classList.add('empty-message');
+        notesEl.appendChild(emptyMessage);
+    }
 
-    filteredNotes.forEach((note) => {
-        const noteEl = generateNoteDOM(note)
-        document.querySelector('#notes').appendChild(noteEl)
-    })
 }
 //Generate the last edited message
 const generateLastEdited = (timeStamp) => `Last edited ${moment(timeStamp).fromNow()}`;
