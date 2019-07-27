bdcb39f617de7de698b34b7459eb08ed72658a25
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-07-27 21:52:02 +0200

    moduling all the app part 2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-import { getNotes } from './notes.js';
+import { getNotes, createNotes, removeNote, updateNote } from './notes.js';
+import { getFilters, setFilters } from './filters'
 
-console.log(getNotes());
+// console.log(getNotes());
+// createNotes()
+// updateNote('f3fc2b4e-8e2a-42ff-bec4-c2f2f1543f99', {
+//     title: 'My note title',
+//     body: 'This is the body for my note'
+// })
+// console.log(getNotes());
+console.log(getFilters());
+setFilters({
+    searchText: 'Office',
+    sortBy: 'bycreated'
+});
+console.log(getFilters());  
