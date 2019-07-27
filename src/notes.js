0cb0c3db1e280ef0dc7eebc5d160d9af6b7cdbb0
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-07-27 17:01:17 +0200

    moduling all the app

---
+++ src/notes.js
@@ -0,0 +1,19 @@
+let notes = [];
+
+// Read existing notes from localStorage
+const loadNotes = () => {
+    const notesJSON = localStorage.getItem('notes')
+
+    try {
+        return notesJSON ? JSON.parse(notesJSON) : []
+    } catch (e) {
+        return []
+    }
+}
+
+//Expose notes from module
+const getNotes = () => notes
+
+notes = loadNotes();
+
+export { getNotes };
