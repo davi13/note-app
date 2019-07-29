48fff08698f9040811182697ec60b64d9f818a3c
Daviel Frias Garcia <yfriasgarcia@gmail.com>
2019-07-29 12:52:10 +0200

    we modify the new index.js and  modeling all the functions that we had in notes.js importer to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import { getNotes, createNotes, removeNote, updateNote } from './notes.js';
-import { getFilters, setFilters } from './filters'
+import { createNotes } from './notes.js';
+import { setFilters } from './filters'
 import { renderNotes } from './views';
 
-renderNotes(notes, filters);
+renderNotes();
 
 document.querySelector('#create-note').addEventListener('click', (e) => {
     const id = createNotes();
@@ -11,18 +11,24 @@ document.querySelector('#create-note').addEventListener('click', (e) => {
 })
 
 document.querySelector('#search-text').addEventListener('input', (e) => {
-    filters.searchText = e.target.value;
+
+    setFilters({
+        searchText: e.target.value
+    })
     renderNotes();
 })
 
 document.querySelector('#filter-by').addEventListener('change', (e) => {
-    filters.sortBy = e.target.value;
+
+    setFilters({
+        sortBy: e.target.value
+    })
     renderNotes();
 })
 
 window.addEventListener('storage', (e) => {
     if (e.key === 'notes') {
-        notes = JSON.parse(e.newValue);
+
         renderNotes();
     }
 })
